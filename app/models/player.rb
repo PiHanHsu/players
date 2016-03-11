0d101bc9a67b865a22543452cc2f6466621ee8fc
PiHan Hsu <pihanhsu@gmail.com>
2016-03-11 23:34:10 +0800

    add validates

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,9 @@
 class Player < ActiveRecord::Base
 	validates_presence_of :first_name
 	validates_presence_of :last_name
+	validates_presence_of :number
+	validates_presence_of :team
+	validates_presence_of :birthday
+	validates_presence_of :position
+	validates_presence_of :experience
 end
